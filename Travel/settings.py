c5f9e683d4480174bfca8c9b6b1ca166fcb6599c
Suruthika <suruthika0704@gmail.com>
2026-09-21 14:09:52 +0530

    Add error logging

--- Travel/settings.py
+++ Travel/settings.py
@@ -171,3 +171,17 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
